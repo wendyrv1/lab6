388629d688b26e8a0b40b2e422d62311f1d792a8
annika930 <annikayun@ufl.edu>
2023-03-21 18:57:09 -0400

    i added the main def thing

--- main.py
+++ main.py
@@ -16,24 +16,25 @@ def decode(password):
     return edp
 
 #display menu with the options
-while True:
-    print('Menu')
-    print('-------------')
-    print('1. Encode')
-    print('2. Decode')
-    print('3. Quit')
-    num = int(input('Please enter an option:'))
-    #encodes the orignal password
-    if num == 1:
-        password = input('Please enter your password to encode:')
-        print('Your password has been encoded and stored!')
-        password = encode(password)
+def main():
+    while True:
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        num = int(input('Please enter an option:'))
+        #encodes the orignal password
+        if num == 1:
+            password = input('Please enter your password to encode:')
+            print('Your password has been encoded and stored!')
+            password = encode(password)
 
-    elif num == 2:
-        print(f'The encoded password is {password}, and the original password is {decode(password)}')
+        elif num == 2:
+            print(f'The encoded password is {password}, and the original password is {decode(password)}')
 
-    elif num == 3:
-        break
+        elif num == 3:
+            break
 
 if __name__ == '__main__':
     main()
